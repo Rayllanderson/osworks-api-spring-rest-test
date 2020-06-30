eef6ae86a6ea038d45d22a0478101474d12f9ece
Rayllanderson <rayllanderson@gmail.com>
2020-06-30 09:41:38 -0300

    Listar e buscar com JPA

--- src/main/java/com/ray/osworks/api/controller/ClientsController.java
+++ src/main/java/com/ray/osworks/api/controller/ClientsController.java
@@ -1,22 +1,33 @@
 package com.ray.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ray.osworks.domain.model.Cliente;
+import com.ray.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClientsController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
     
-    @GetMapping("/clients")
+    @GetMapping("/clientes")
     public List<Cliente> listar () {
-	var cliente1 = new Cliente(1L, "João Casmurro", "joao@gmail.com", "+989899898");
-	var cliente2 = new Cliente(2l, "Maria Casmurra", "maria@gmail.com", "989989565");
-	
-	return Arrays.asList(cliente1, cliente2);
+	return clienteRepository.findAll();
+	//return clienteRepository.findByNomeContaining("Casmurr");
     }
 
+    @GetMapping("/clientes/{clienteId}")
+    public Cliente buscar(@PathVariable Long clienteId) {
+	Optional <Cliente> cliente = clienteRepository.findById(clienteId);
+	
+	//se n tiver nada, retorna nulo
+	return cliente.orElse(null);
+    }
 }
--- src/main/java/com/ray/osworks/domain/model/Cliente.java
+++ src/main/java/com/ray/osworks/domain/model/Cliente.java
@@ -1,18 +1,30 @@
 package com.ray.osworks.domain.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    
+    private String nome;
     private String email;
+    
+    @Column(name = "fone")
     private String telefone;
     
     public Cliente () {}
 
-    public Cliente(Long id, String name, String email, String telefone) {
-	super();
+    public Cliente(Long id, String nome, String email, String telefone) {
 	this.id = id;
-	this.name = name;
+	this.nome = nome;
 	this.email = email;
 	this.telefone = telefone;
     }
@@ -25,12 +37,12 @@ public class Cliente {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getNome() {
+        return nome;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setNome(String name) {
+        this.nome = name;
     }
 
     public String getEmail() {
@@ -47,8 +59,31 @@ public class Cliente {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Cliente other = (Cliente) obj;
+	if (id == null) {
+	    if (other.id != null)
+		return false;
+	} else if (!id.equals(other.id))
+	    return false;
+	return true;
     };
-
-
 
 }
